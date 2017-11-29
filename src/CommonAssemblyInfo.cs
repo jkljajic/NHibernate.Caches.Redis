cad40516e74be08c9fc3f546a5be02bcc058d2aa
Jovo Kljajic <jovo.kljajic@seavus.com>
2017-11-29 10:53:43 +0100

    Renable .net40 project back in

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
 [assembly: AssemblyCompany("http://adrianphinney.com")]
 [assembly: AssemblyCopyright("Copyright © Harbour Inc. 2015")]
 [assembly: ComVisible(false)]
